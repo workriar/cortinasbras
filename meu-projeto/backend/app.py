7c2798ff993a36816b2c9ebc1497e9dbc646baa0
root <root@srv893695.hstgr.cloud>
2025-10-28 21:42:27 +0000

    Corrige React 18 e paths do Flask

--- meu-projeto/backend/app.py
+++ meu-projeto/backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
 import os
 
-app = Flask(__name__, static_folder='../frontend/build')
+app = Flask(__name__, static_folder='build', static_url_path='')
 CORS(app)
 
 # Configuração
